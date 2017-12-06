a27d461bceb41594601aad34167a0197908836a2
chrisni123 <chrisni@udel.edu>
2017-12-05 19:17:14 -0500

    added movies table

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # group-assignment-01
 Group Members:
-+ Sam Rosen
-+ Mike Caffrey
-+ Christine Ni
+  * Sam Rosen
+  * Mike Caffrey
+  * Christine Ni
 
--Our team project is projected to be a movie database that will consist of two tables with a one to many relationship.
+- Our team project is projected to be a movie database that will consist of two tables with a one to many relationship.
 The first table will be a movie table with a title, director, year, genre, and description columns.
 The second table will be an actor table with name, age, and gender columns
 
 - The Movie Table would look like this
-+Movie Id | Movie Title | Director | Year | Genre | Description
-+---------+-------------+----------+------+-------+----------
-+ 1 *Movie Title* |Director| Year |Genre| Description
+Movie Id | Movie Title | Director | Year | Genre | Description
+---------|-------------|----------|------|-------|------------
+1| *Movie Title* |Director|  Year    View    Edit |Genre| Description
+2| *Finding Dory* |Ellen DeGeneres| 2017| Family| Please refer to our movie description database.
 
 - The Actor Table would look like this
 +Actor Id | Actor Name | Age | Gender
-+ ------------ | -------------
-+ Content from cell 1 | Content from cell 2
-+ Content in the first column | Content in the second column
+----------|------------|-----|-------
+1|	Jane Austen|	Jane Austen was an English novelist known primarily for her six major novels, which interpret, critique and comment upon the British landed gentry at the end of the 18th century.	Edit
+2	|J. K. Rowling	|Joanne Rowling, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who wrote the Harry Potter fantasy series.
 
 
 Link to our GitHub
--- project.py
+++ project.py
@@ -11,6 +11,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 
+# defining the database tables
 class Movie(db.Model):
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
@@ -22,15 +23,37 @@ def index():
     return render_template('index.html')
 
 @app.route('/movies')
-def get_all_movies():
-    movies =[
-    'The Lion King',
-    'Mulan',
-    'Moana',
-    'Frozen',
-    'Cinderella'
-    ]
-    return render_template('movies.html', movies = movies)
+def show_all_movies():
+    movies = Movie.query.all()
+    return render_template('movies-all.html', movies=movies)
+
+@app.route('/movie/add', methods=['GET', 'POST'])
+def add_movies():
+    if request.method == 'GET':
+        return render_template('movies-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        intro = request.form['intro']
+
+        # insert the data into the database
+        movie = Movie(name=name, intro=intro)
+        db.session.add(movie)
+        db.session.commit()
+        return redirect(url_for('show_all_movies'))
+
+@app.route('/movie/edit/<int:id>', methods=['GET', 'POST'])
+def edit_movie(id):
+    movie = Movie.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('movie-edit.html', movie=movie)
+    if request.method == 'POST':
+        # update data based on the form data
+        movie.title = request.form['title']
+        movie.description = request.form['description']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_movies'))
 
 @app.route('/members')
 def members_page():
